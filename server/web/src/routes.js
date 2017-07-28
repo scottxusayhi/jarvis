662fa381b38f0a4eb395540312a9497fecd53289
Di Xu <xudi@ict.ac.cn>
2017-07-28 19:29:08 +0800

    add react web

---
+++ server/web/src/routes.js
@@ -0,0 +1,10 @@
+// these route records is for generating breadcrumb headers
+// has nothing to do with the react-route itself
+
+const routes = {
+    '/': '首页',
+    '/dashboard': '总览',
+    '/hosts': '主机',
+
+};
+export default routes;
